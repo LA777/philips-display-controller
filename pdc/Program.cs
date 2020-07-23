5ae2191d82d844fa6eb6b5b433b888b0df96c828
Alexander Lazarenko <23220049+LA777@users.noreply.github.com>
2020-07-23 05:31:57 +0300

    added DLLsand changed to .Net Framework 4.8

--- pdc/Program.cs
+++ pdc/Program.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using CSDDCHelperAPILib;
+using System;
+using System.Collections.Generic;
+using System.Management;
 
 namespace pdc
 {
     class Program
     {
+        public static List<string> displayNames = new List<string>();
+        public static string currentDisplayName = string.Empty;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ShowAvailableDisplayInfo();
+        }
+
+        private static void ShowAvailableDisplayInfo()
+        {
+            InitDisplayInfo();
+
+            for (int index = 0; index < displayNames.Count; index++)
+            {
+                Console.WriteLine($"Display index: {index} | Display name: {displayNames[index]}");
+            }
+        }
+
+        private static void InitDisplayInfo()
+        {
+            DDCHelperAPI.DDCCIHelperIni_CS();
+            var displayIdIndex = DDCHelperAPI.EnumDisplayIDIni_CS(ref displayNames, ref currentDisplayName);
+        }
+
+        private static void SetDisplayBrightness(int displayIndex, int brightness)
+        {
+            InitDisplayInfo();
+            DDCHelperAPI.setStandardDDCCIValue_CS(displayIndex, CSDDCHelperAPILib.VCPCodeCmd.CGMenuStdVCPCmd.eVCPOpCode_E.OP_10_Luminance, brightness);
         }
     }
 }
