28520aacd5cbf57bc98be1824c0af563a72d41c1
Alexander Lazarenko <23220049+LA777@users.noreply.github.com>
2020-07-23 04:22:34 +0300

    initial commit

---
+++ pdc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pdc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
